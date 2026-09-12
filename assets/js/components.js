4bc96e94001d76eb452e8ff7d54e4b6504771851
Yakka Works <yakkaworks@gmail.com>
2026-09-12 11:08:23 +0700

    Update components.js

--- assets/js/components.js
+++ assets/js/components.js
@@ -1,3 +1,5 @@
+document.documentElement.setAttribute('data-bs-theme', 'dark');
+
 const YW_NAV_ITEMS = [
   {
     href: '/store/',
@@ -46,13 +48,13 @@ function renderNavbar() {
   `).join('');
 
   mount.innerHTML = `
-    <nav class="navbar" style="background-color: var(--bs-primary);">
-      <div class="container-fluid">
-        <a class="navbar-brand d-flex align-items-center gap-2 text-white" href="/">
+    <nav class="navbar yw-navbar">
+      <div class="container-fluid yw-navbar-inner">
+        <a class="navbar-brand d-flex align-items-center gap-2" href="/">
           <img src="/assets/img/logo.svg" alt="Yakka Works" height="32" width="32">
           <span class="d-none d-sm-inline">Yakka Works</span>
         </a>
-        <button class="btn btn-link text-white p-0 border-0" type="button"
+        <button class="btn btn-link p-0 border-0 yw-navbar-toggle" type="button"
           data-bs-toggle="offcanvas" data-bs-target="#yw-offcanvas"
           aria-controls="yw-offcanvas" aria-label="Open navigation menu">
           <i class="bi bi-list fs-2"></i>
@@ -63,7 +65,7 @@ function renderNavbar() {
     <div class="offcanvas offcanvas-end" tabindex="-1" id="yw-offcanvas" aria-labelledby="yw-offcanvas-label">
       <div class="offcanvas-header">
         <h5 class="offcanvas-title" id="yw-offcanvas-label">Yakka Works</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
       <div class="offcanvas-body p-0">
         <div class="list-group list-group-flush">
@@ -79,11 +81,11 @@ function renderFooter() {
   if (!mount) return;
 
   mount.innerHTML = `
-    <footer class="text-white py-5 mt-5" style="background-color: var(--bs-primary);">
+    <footer class="yw-footer py-5 mt-3">
       <div class="container">
         <div class="row gy-4">
           <div class="col-12 col-md-4">
-            <a href="/" class="d-flex align-items-center gap-2 text-white text-decoration-none mb-2">
+            <a href="/" class="d-flex align-items-center gap-2 text-decoration-none mb-2 yw-footer-brand">
               <img src="/assets/img/logo.svg" alt="Yakka Works" height="28" width="28">
               <span class="fw-medium">Yakka Works</span>
             </a>
@@ -99,30 +101,30 @@ function renderFooter() {
           <div class="col-6 col-md-4">
             <h6 class="text-uppercase small mb-3">Legal</h6>
             <ul class="list-unstyled small">
-              <li class="mb-2"><a href="/terms.html" class="text-white text-decoration-none">Terms</a></li>
-              <li class="mb-2"><a href="/privacy.html" class="text-white text-decoration-none">Privacy</a></li>
-              <li class="mb-2"><a href="/license.html" class="text-white text-decoration-none">License</a></li>
-              <li class="mb-2"><a href="https://github.com/yakkaworks" class="text-white text-decoration-none">Source Code</a></li>
+              <li class="mb-2"><a href="/terms.html" class="yw-footer-link">Terms</a></li>
+              <li class="mb-2"><a href="/privacy.html" class="yw-footer-link">Privacy</a></li>
+              <li class="mb-2"><a href="/license.html" class="yw-footer-link">License</a></li>
+              <li class="mb-2"><a href="https://github.com/yakkaworks/yakkaworks.github.io" class="yw-footer-link">Source Code</a></li>
             </ul>
           </div>
 
           <div class="col-6 col-md-4">
             <h6 class="text-uppercase small mb-3">Connect</h6>
             <div class="d-flex gap-3 fs-5 mb-3">
-              <a href="https://facebook.com/yakkaworks" class="text-white" aria-label="Facebook"><i class="bi bi-facebook"></i></a>
-              <a href="https://youtube.com/@yakkaworks" class="text-white" aria-label="YouTube"><i class="bi bi-youtube"></i></a>
-              <a href="https://github.com/yakkaworks" class="text-white" aria-label="GitHub"><i class="bi bi-github"></i></a>
-              <a href="https://x.com/yakkaworks" class="text-white" aria-label="X"><i class="bi bi-twitter-x"></i></a>
-              <a href="https://instagram.com/yakkaworks" class="text-white" aria-label="Instagram"><i class="bi bi-instagram"></i></a>
+              <a href="https://facebook.com/yakkaworks" class="yw-footer-link" aria-label="Facebook"><i class="bi bi-facebook"></i></a>
+              <a href="https://youtube.com/@yakkaworks" class="yw-footer-link" aria-label="YouTube"><i class="bi bi-youtube"></i></a>
+              <a href="https://github.com/yakkaworks" class="yw-footer-link" aria-label="GitHub"><i class="bi bi-github"></i></a>
+              <a href="https://x.com/yakkaworks" class="yw-footer-link" aria-label="X"><i class="bi bi-twitter-x"></i></a>
+              <a href="https://instagram.com/yakkaworks" class="yw-footer-link" aria-label="Instagram"><i class="bi bi-instagram"></i></a>
             </div>
-            <a href="mailto:yakkaworks@gmail.com" class="text-white small text-decoration-none d-block">
+            <a href="mailto:yakkaworks@gmail.com" class="yw-footer-link small d-block">
               <i class="bi bi-envelope me-1"></i>yakkaworks@gmail.com
             </a>
           </div>
         </div>
 
-        <hr class="border-white-50 my-4">
-        <p class="small mb-0">Copyright Yakka Works 2019–2026</p>
+        <hr class="yw-footer-hr my-4">
+        <p class="small mb-0">Copyright Yakka Works 2019-2026</p>
       </div>
     </footer>
   `;
